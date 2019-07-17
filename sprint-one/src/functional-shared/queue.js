14fc8af6a8eba8c0f2ed67a182d5a0e9b23d1886
omega1984 <omegareborn2019@gmail.com>
2019-07-17 15:49:57 -0700

    completed shared queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,30 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.storage = {};
+  someInstance.max = 0;
+  someInstance.min = 0;
+  _.extend(someInstance, queueMethods);
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value){
+    this.storage[this.max] = value;
+    this.max++;
+  },
+  dequeue: function(){
+    if (this.max > this.min){
+      var deletedItem = this.storage[this.min];
+      delete this.storage[this.min];
+      this.min++;
+    }else if (this.min >= this.max){
+      return -1;
+    }
+    return deletedItem;
+  },
+  size: function(){
+    return this.max - this.min;
+  }
+};
 
 
