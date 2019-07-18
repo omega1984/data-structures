e2e6c793b3a3b0fb6f824e53c3f07fa1f4fa888f
omega1984 <omegareborn2019@gmail.com>
2019-07-18 15:48:35 -0700

    tree completed

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,14 +4,40 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    if (!list.head){
+      list.head = newNode;
+      list.tail = list.head;
+    }else{
+      list.tail.next = newNode;
+      list.tail = newNode;
+    }
+    return list;
   };
 
   list.removeHead = function() {
+    if (!list.head) return undefined;
+    var current = list.head;
+    list.head = current.next;
+    if (!list.head){
+      list.head = null;
+      list.tail = null;
+    }
+    return current.value;
   };
 
   list.contains = function(target) {
-  };
+    var current = list.head;
 
+    while (current){
+      if (current.value !== target){
+        current = current.next;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  };
   return list;
 };
 
