ade84d2c03f83e53533ab6889078115ca64debb0
omega1984 <omegareborn2019@gmail.com>
2019-07-17 17:36:32 -0700

    try to finish part 3

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,29 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.storage = {};
+  someInstance.count = 0;
+  _.extend(someInstance, stackMethods);
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value){
+    this.storage[this.count] = value;
+    this.count++;
+  },
+  pop: function(){
+    if (this.count > 0){
+      var deletedItem = this.storage[this.count-1];
+      delete this.storage[this.count-1];
+      this.count--;
+    }else{
+      return undefined;
+    }
+    return deletedItem;
+  },
+  size: function(){
+    return this.count;
+  }
+};
 
 
--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,9 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(Queue.method);
+  someInstance.max = 0;
+  someInstance.min = 0;
+  return someInstance;
 };
+
 
 
