4c424b006fe1f18efe18d4b310035954ef9afb56
omega1984 <omegareborn2019@gmail.com>
2019-07-18 22:34:27 -0700

    graph completed

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,37 +2,58 @@
 
 // Instantiate a new graph
 var Graph = function() {
-    this.adjacencyList = {};
+    this.nodes = {};
+    this.edges = {};
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
-    var dict = this.adjacencyList;
-    if (!dict[node]) dict[node] = [];
+    this.nodes[node] = node;
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+    if (this.nodes[node] === node) {
+        return true;
+    }
+    return false;
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+    delete this.nodes[node];
+    delete this.edges[node];
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+    if (this.edges[fromNode] === fromNode && this.edges[toNode] === toNode){
+        return true;
+    }
+    return false;
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+    if (!this.edges[fromNode]){
+        this.edges[fromNode] = fromNode;
+    }
+    if (!this.edges[toNode]){
+        this.edges[toNode] = toNode;
+    }
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+    delete this.edges[fromNode];
+    delete this.edges[toNode];
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+    for (var node in this.nodes){
+        cb(this.nodes[node]);
+    }
 };
 
 /*
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -23,4 +23,5 @@ setPrototype.remove = function(item) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ all O(1);
  */
