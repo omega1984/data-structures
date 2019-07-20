33d3c5e33f71611b8efdebe8ccdd5eb42fe098fe
omega1984 <omegareborn2019@gmail.com>
2019-07-19 18:43:07 -0700

    hashtable complete

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -6,29 +6,32 @@ var HashTable = function() {
 };
 
 HashTable.prototype.insert = function(k, v) {
+  // k is the string, v is the length of string;
+  // we pass k and v into hash function to get a unique code;
+  // unique code indicates the location of the bucket that we are going to store in the array;
   var index = getIndexBelowMaxForKey(k, this._limit);
-  var bucket = this._storage[index];
-
-  if (!bucket) {
-    var newBucket = [];
-    this._storage[index] = newBucket;
-  }
-  if (this._storage[index].length > 0){
-    for (var tuple = 0; tuple < this._storage[index].length; tuple++){
-      if (this._storage[index][tuple][0] === k){
-        this._storage[index][tuple][1] = v;
+  var container = this._storage;
+  if (!container.get(index)){
+    var bucket = [[k, v]];
+    container.set(index, bucket);
+  }else{
+    var existBucket = container.get(index);
+    for (var i = 0; i < existBucket.length; i++){
+      if (existBucket[i][0] === k){
+        existBucket[i][1] = v;
+      } else if (i === existBucket.length - 1) {
+        existBucket.push([k, v]);
       }
     }
   }
-  bucket = this._storage[index];
-  bucket.push([k, v]);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  var bucket = this._storage[index];
-  for (var i = 0; i < bucket.length; i++) {
-    if (bucket[i][0] === k) {
+  var container = this._storage;
+  var bucket = container.get(index);
+  for (var i = 0; i < bucket.length; i++){
+    if (bucket[i][0] === k){
       return bucket[i][1];
     }
   }
@@ -36,14 +39,16 @@ HashTable.prototype.retrieve = function(k) {
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  var bucket = this._storage[index];
+  var container = this._storage;
+  var bucket = container.get(index);
+
   if (!bucket){
-    return null;
-  }
-  for (var i = 0; i < bucket.length; i++){
-    var tuple = bucket[i];
-    if (tuple[0] === k) {
-      bucket.splice(i, 1);
+    return undefined;
+  }else{
+    for (var i = 0; i < bucket.length; i++){
+      if (bucket[i][0] === k) {
+        bucket.splice(i, 1);
+      }
     }
   }
 };
@@ -53,7 +58,7 @@ HashTable.prototype.remove = function(k) {
 /*
  * Complexity: What is the time complexity of the above functions?
 
- O(1);
+ O(n);
  */
 
 
